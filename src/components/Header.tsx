4a654c146707c9f4a250e35a936ab01111686090
Paul <paulpastor122297@gmail.com>
2024-03-05 09:38:49 +0800

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Header.module.css'
+const {} = styles
+
+function Header(props: any) {
+    const {} = props
+    
+    return (
+        <header>
+            <h1>Store API</h1>
+
+        </header>
+    )
+}
+
+export default Header
